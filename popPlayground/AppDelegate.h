6fe532e5991b2120015c1d3d8fc7ab96ec66f150
Edwin Aaron Saenz <aaronxsaenz@gmail.com>
2014-10-03 21:46:24 -0500

    Initial Commit

---
+++ popPlayground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  popPlayground
+//
+//  Created by Edwin Aaron Saenz on 10/3/14.
+//  Copyright (c) 2014 Edwin Aaron Saenz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ popPlayground/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  popPlayground
+//
+//  Created by Edwin Aaron Saenz on 10/3/14.
+//  Copyright (c) 2014 Edwin Aaron Saenz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
