9f500327d80d3c1ccad7bceefb07b42d8b82e824
Edwin Aaron Saenz <aaronxsaenz@gmail.com>
2014-10-09 22:00:03 -0500

    Implemented POPDecayAnimation for Pan Gesture

---
+++ popPlayground/CircleView.h
@@ -0,0 +1,13 @@
+//
+//  CircleView.h
+//  popPlayground
+//
+//  Created by Edwin Aaron Saenz on 10/3/14.
+//  Copyright (c) 2014 Edwin Aaron Saenz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleView : UIView
+
+@end
